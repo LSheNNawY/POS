95f5974525eb3d248a6030d310839a4e803e2314
LSheNNawY <devshennawy@gmail.com>
2019-04-22 06:57:49 +0200

    adding and configuring laratrust

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -2,24 +2,12 @@
 
 return [
     'role_structure' => [
-        'superadministrator' => [
+        'super_admin' => [
             'users' => 'c,r,u,d',
-            'acl' => 'c,r,u,d',
-            'profile' => 'r,u'
-        ],
-        'administrator' => [
-            'users' => 'c,r,u,d',
-            'profile' => 'r,u'
-        ],
-        'user' => [
-            'profile' => 'r,u'
-        ],
+
+        ]
     ],
-    'permission_structure' => [
-        'cru_user' => [
-            'profile' => 'c,r,u'
-        ],
-    ],
+
     'permissions_map' => [
         'c' => 'create',
         'r' => 'read',
--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -10,5 +10,7 @@ Route::group(['prefix' => LaravelLocalization::setLocale(),
         })->name('index');
     });
 
+
+
 });
 
